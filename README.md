d90bd79cd01105136a966e7e3a7e1fbe1f0d28fc
Dinkar111 <maharjandinkar1@gmail.com>
2021-06-22 11:18:04 +0545

    -[x] SMS-2 Completed -[x] Naming convention changed -[x] Password doesnot show in CLI -[x] Try/catch added -[x] Basic CRUD operation complete

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # school-management-system
+
+### Install pip-tools to sync and install from requirements.txt
+
+#### To install pip-tools
+
+	pip3 install pip-tools
+
+#### To install from requirements.txt
+
+	pip-sync
+
+#### To install dev-requirements.txt
+
+	pip-sync dev-requirements.txt
+		
+
--- main.py
+++ main.py
@@ -1,20 +1,55 @@
-from db_config import DBConfigClass
+from config import DBConfigClass
 from database import DbClass
+from view.cli_screen import CliScreen
+import sys
 
 
 class Main:
 
     def __init__(self):
-
-        # Config file 
+        # Config file
         db_config = DBConfigClass()
 
-        # database config from config file is return in python dictionary
-        db_data = db_config.get_db_config()
+        # get db config data from DBConfigClass() object
+        db_data = db_config.get_config()
 
-        # initiate DbClass 
-        self.db =  DbClass(db_data['db'], db_data['user'], db_data['passwd'], db_data['host'])
-        
+        # initiate DbClass
+        self.db = DbClass(
+            db_name=db_data["db"],
+            user_name=db_data["user"],
+            password=db_data["password"],
+            host_name=db_data["host"]
+        )
+        self.db.connect()
+
+        # initiate screen class object
+        self.cli_screen = CliScreen(self.db)
+
+        # choices to select in interface which action to run
+        self.choices = {
+            "1": self.cli_screen.login_view,
+            "2": self.cli_screen.register_view,
+            "3": sys.exit
+        }
+
+    def main(self):
+
+        print("SCHOOL MANAGEMENT SYSTEM")
+        print("1. Login")
+        print("2. Register")
+        print("3. Exit")
+        try:
+            choice = input("\nWhat would you like to do? Please choose: \n")
+            action = self.choices.get(choice)
+            if action:
+                action()
+                self.main()
+            else:
+                print("Thank you, Goodbye")
+        except KeyboardInterrupt as e:
+            print("\nThank you, Goodbye\n")
+
      
-if __name__== "__main__":
-    Main()
+if __name__ == "__main__":
+    run = Main()
+    run.main()
